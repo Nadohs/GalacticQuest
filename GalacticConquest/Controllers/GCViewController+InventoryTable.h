e9e735f557ec707416d1fd5acdce5a01724a7662
Bob Odenkirk <djsente@gmail.com>
2014-03-15 15:28:37 -0700

    added inventory and shrinking astroids

---
+++ GalacticConquest/Controllers/GCViewController+InventoryTable.h
@@ -0,0 +1,13 @@
+//
+//  GCViewController+InventoryTable.h
+//  GalacticConquest
+//
+//  Created by Rich on 3/15/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "GCViewController.h"
+
+@interface GCViewController (InventoryTable)
+
+@end
---
+++ GalacticConquest/Controllers/GCViewController+InventoryTable.m
@@ -0,0 +1,13 @@
+//
+//  GCViewController+InventoryTable.m
+//  GalacticConquest
+//
+//  Created by Rich on 3/15/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "GCViewController+InventoryTable.h"
+
+@implementation GCViewController (InventoryTable)
+
+@end
