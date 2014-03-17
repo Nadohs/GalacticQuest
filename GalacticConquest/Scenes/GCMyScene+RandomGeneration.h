9a2add2471a7a61896dd7d93517c362132b9e45f
Bob Odenkirk <djsente@gmail.com>
2014-03-17 16:23:56 -0700

    collisions will only check localized astrials

---
+++ GalacticConquest/Scenes/GCMyScene+RandomGeneration.h
@@ -0,0 +1,13 @@
+//
+//  GCMyScene+RandomGeneration.h
+//  GalacticConquest
+//
+//  Created by Rich on 3/17/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "GCMyScene.h"
+
+@interface GCMyScene (RandomGeneration)
+
+@end
---
+++ GalacticConquest/Scenes/GCMyScene+RandomGeneration.m
@@ -0,0 +1,13 @@
+//
+//  GCMyScene+RandomGeneration.m
+//  GalacticConquest
+//
+//  Created by Rich on 3/17/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "GCMyScene+RandomGeneration.h"
+
+@implementation GCMyScene (RandomGeneration)
+
+@end
