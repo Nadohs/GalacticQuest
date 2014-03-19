ff791344f4b649986503b25bf33b8cae21539d8e
Bob Odenkirk <djsente@gmail.com>
2014-03-19 13:46:55 -0700

    update license

---
+++ GalacticConquest/astrials/SpaceStation.h
@@ -0,0 +1,13 @@
+//
+//  SpaceStation.h
+//  GalacticConquest
+//
+//  Created by Rich on 3/17/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "AstrialObject.h"
+
+@interface SpaceStation : AstrialObject
+
+@end
---
+++ GalacticConquest/astrials/SpaceStation.m
@@ -0,0 +1,13 @@
+//
+//  SpaceStation.m
+//  GalacticConquest
+//
+//  Created by Rich on 3/17/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "SpaceStation.h"
+
+@implementation SpaceStation
+
+@end
