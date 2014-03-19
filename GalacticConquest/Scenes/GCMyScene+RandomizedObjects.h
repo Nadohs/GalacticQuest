14772b6108e70e134ff8fdd928df7a5375d747c1
Bob Odenkirk <djsente@gmail.com>
2014-03-19 15:34:53 -0700

    moved randomized astrial creation to category

---
+++ GalacticConquest/Scenes/GCMyScene+RandomizedObjects.h
@@ -0,0 +1,13 @@
+//
+//  GCMyScene+RandomizedObjects.h
+//  GalacticConquest
+//
+//  Created by Rich on 3/19/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "GCMyScene.h"
+
+@interface GCMyScene (RandomizedObjects)
+
+@end
---
+++ GalacticConquest/Scenes/GCMyScene+RandomizedObjects.m
@@ -0,0 +1,13 @@
+//
+//  GCMyScene+RandomizedObjects.m
+//  GalacticConquest
+//
+//  Created by Rich on 3/19/14.
+//  Copyright (c) 2014 NadohsInc. All rights reserved.
+//
+
+#import "GCMyScene+RandomizedObjects.h"
+
+@implementation GCMyScene (RandomizedObjects)
+
+@end
